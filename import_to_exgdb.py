4e8e7e491ab901cc878ddcb5f06d3e4fa084ff98
miyagaw61 <miyagaw61@gmail.com>
2019-09-09 19:41:30 +0900

    Use Exgdb.THISDIR

--- import_to_exgdb.py
+++ import_to_exgdb.py
@@ -1,8 +1,4 @@
-cmds = [cmd for cmd in dir(SampleCmd) if callable(getattr(SampleCmd, cmd))]
-for cmd in cmds:
-    if not cmd.startswith("_"):
-        cmd_obj = getattr(SampleCmd, cmd)
-        setattr(ExgdbCmd, cmd, cmd_obj)
-#for cmd in samplecmd.cmds:
-#    cmd_obj = getattr(MyPlugin, cmd)
-#    setattr(ExgdbCmd, cmd, cmd_obj)
+gdb.execute("source %s/sample_plugin.py" % Exgdb.THISDIR)
+for cmd in samplecmd.cmds:
+    cmd_obj = getattr(samplecmd, cmd)
+    setattr(ExgdbCmd, cmd, cmd_obj)
